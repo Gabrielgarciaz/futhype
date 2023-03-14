43428e30098b3f26a68cb7c128552052290c973c
Gabriel <gabrielgarciaofc@gmail.com>
2023-03-14 13:10:57 -0300

    Implementando o retorno com Page

--- src/main/java/com/gabrielgarcia/futhype/controllers/ProdutoController.java
+++ src/main/java/com/gabrielgarcia/futhype/controllers/ProdutoController.java
@@ -1,5 +1,9 @@
 package com.gabrielgarcia.futhype.controllers;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +16,16 @@ import com.gabrielgarcia.futhype.services.ProdutoService;
 @RequestMapping(value = "/produtos")
 public class ProdutoController {
 	
+	@Autowired
 	private ProdutoService service;
 	
 	@GetMapping(value = "/{id}")
 	public ProdutoDTO findById(@PathVariable Long id) {
 		return service.findById(id);
+	}
+	
+	@GetMapping
+	public Page<ProdutoDTO> findAll(Pageable pagina) {
+		return service.findAll(pagina);
 	}
 }
--- src/main/java/com/gabrielgarcia/futhype/services/ProdutoService.java
+++ src/main/java/com/gabrielgarcia/futhype/services/ProdutoService.java
@@ -1,19 +1,14 @@
 package com.gabrielgarcia.futhype.services;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.gabrielgarcia.futhype.dto.ProdutoDTO;
 import com.gabrielgarcia.futhype.entities.Produto;
 import com.gabrielgarcia.futhype.repositories.ProdutoRepository;
-
-
-
-
-
 
 
 @Service
@@ -24,10 +19,13 @@ public class ProdutoService {
 	
 	@Transactional(readOnly = true)
 	public ProdutoDTO findById(Long id) {
-		Optional<Produto> resultado = produtoRepository.findById(id);
-		Produto produto = resultado.get();
-		ProdutoDTO dto = new ProdutoDTO(produto);
-		return dto;
+		Produto produto = produtoRepository.findById(id).get();
+		return  new ProdutoDTO(produto);
 	}
 	
+	@Transactional(readOnly = true)
+	public Page<ProdutoDTO> findAll(Pageable pagina) {
+		Page<Produto> produtos = produtoRepository.findAll(pagina);
+		return produtos.map(x -> new ProdutoDTO(x));
+	}
 }
